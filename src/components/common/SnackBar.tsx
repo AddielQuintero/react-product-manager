061103fa0f28328c59852248f1583b8b9ba50618
atamayo <atamayoquintero@gmail.com>
2023-04-29 18:05:03 -0500

    feat: add SnackBar component

--- src/components/common/SnackBar.tsx
+++ src/components/common/SnackBar.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { CustomButton } from './Button'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const CustomSnackBar = () => {
   return (
-    <div className="flex justify-between fixed top-8 left-1/2 transform -translate-x-1/2 z-20 w-64 rounded-md bg-gray-800 p-4 text-base leading-5 text-white">
-      Alert primary
-      <CustomButton>X</CustomButton>
+    <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   )
 }
